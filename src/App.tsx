ab74c23a66eb1f9d737f6fbd1cb2e29d98b020d4
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-06-24 18:37:14 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-red-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam vero saepe, officia iure pariatur repellat. Minus consectetur voluptas sequi sapiente, in doloremque dolorem doloribus, nihil ad voluptate nemo distinctio perspiciatis?</p>
+    </>
+  )
+}
+
+export default App
