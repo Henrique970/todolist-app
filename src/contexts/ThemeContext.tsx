44d26e115dc8e799c9b79f168f4670469004bbe7
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-07-08 18:09:56 -0300

    fizx: rosolve erros de tipagem no context API

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,15 +1,25 @@
 import { createContext, useState } from "react";
 
+// Tipando o nome do tema, mostrando ao TP que o tema só tem duas opções.
+// Quando queremos usar unionType, é recomendado usar o type ao invés do interface.
+type ThemeName = "dark" | "light";
+
+interface ThemeContextType {
+    theme: ThemeName;
+    toggleTheme: () => void;
+}
+
 // Criando o contexto do tema.
-export const ThemeContext = createContext({});
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: "dark",
+    toggleTheme: () => {}
+});
 
 // Aqui estamos tipando para o TypeScript entender que o children é React.ReactNode. Ou seja, qualquer elemento React que possa ser renderizado dentro do provedor.
 interface ThemeProviderProps {
     children: React.ReactNode;
 }
-// Tipando o nome do tema, mostrando ao TP que o tema só tem duas opções.
-// Quando queremos usar unionType, é recomendado usar o type ao invés do interface.
-type ThemeName = "dark" | "light";
+
 
 // Criando o provedor do tema.
 // O children é uma propriedade do React que representa os elementos filhos que serão renderizados dentro do provedor.
