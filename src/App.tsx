05fe9898ba1095551b24fa29af707acc2287f40d
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-07-09 16:13:40 -0300

    Feat; mais práticas de code clear, deixando o App.tsx enxuto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import { useContext } from "react"
 import TodoForm from "./components/TodoForm"
 import TodoHeader from "./components/TodoHeader"
 import TodoList from "./components/TodoList"
-import { themeConfig } from "./contexts/theme"
-import { ThemeContext } from "./contexts/ThemeContext"
+import { TodoContainer } from "./components/TodoContainer"
 
 function App() {
 
-  const {theme} = useContext(ThemeContext);
-
   return (
     <>
-      <div className={`${themeConfig[theme].layout.backgroundColor} h-screen`}>
-        <div className={`${themeConfig[theme].layout.heroClass} h-80 bg-cover bg-center`}>
-          <div className="max-w-175 m-auto p-8">
-            <TodoHeader></TodoHeader>
-            <TodoForm></TodoForm>
-            <TodoList></TodoList>
-          </div>
-        </div>
-      </div>
+      <TodoContainer>
+        <TodoHeader></TodoHeader>
+        <TodoForm></TodoForm>
+        <TodoList></TodoList>
+      </TodoContainer>
     </>
   )
 }
